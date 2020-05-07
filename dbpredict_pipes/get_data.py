1b08d60b6ed457401f99f60c3f08cd2e1ab32373
Suhail Thahir <suhail.thahir@gmail.com>
2020-05-06 23:23:45 -0400

    minor readibility changes in naming conventions

--- dbpredict_pipes/get_data.py
+++ dbpredict_pipes/get_data.py
@@ -180,15 +180,15 @@ def get_sql_inputs(data_type,criteria,enrollee_qry):
     
     
     elif data_type=='specialties':
-        pwr50_keys = criteria['phys_codes']
+        ama_keys = criteria['phys_codes']
         
         key_ama_path = str(xwalk_path) + '/AMA_spec.pickle' 
         key_to_ama = pd.read_pickle(key_ama_path)
-        ama_specs = list(key_to_ama[key_to_ama['pwr_key'].isin(pwr50_keys)]['AMA_Equivalent'])
+        ama_specs = list(key_to_ama[key_to_ama['pwr_key'].isin(ama_keys)]['AMA_Equivalent'])
         
         ama_power_path = str(xwalk_path) + '/power_to_AMA.pickle'
         ama_to_power = pd.read_pickle(ama_power_path)
-        power_specs = list(ama_to_power[ama_to_power['AMA_Equivalent'].isin(ama_specs)]['power top 50'])
+        power_specs = list(ama_to_power[ama_to_power['AMA_Equivalent'].isin(ama_specs)]['power_names'])
         power_str = str(power_specs)[1:-1]
         
         ama_hr_path = str(xwalk_path) + '/healthrules_to_AMA.pickle'
