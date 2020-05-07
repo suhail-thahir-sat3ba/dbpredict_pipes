e1c271cf4b0239670990c60b44d64655a84796fc
bperry12 <bperry12@users.noreply.github.com>
2020-05-06 22:09:19 -0400

    Update dbpredict_pipes/get_data.py
    Co-authored-by: suhail-thahir-sat3ba <61067815+suhail-thahir-sat3ba@users.noreply.github.com>

--- dbpredict_pipes/get_data.py
+++ dbpredict_pipes/get_data.py
@@ -201,7 +201,7 @@ def get_sql_inputs(data_type,criteria,enrollee_qry):
         pass
     
     
-    sql_inputs = sql_inputs.update({'enrollee_qry' : enrollee_qry})
+    sql_inputs.update({'enrollee_qry' : enrollee_qry})
     
     return sql_inputs
     
