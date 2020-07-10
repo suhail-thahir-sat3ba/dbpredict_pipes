e91e3eadb208fb7f2aa520b03d7ac5646d0896cd
bperry12 <bperry12@gmail.com>
2020-07-10 09:24:49 -0400

    release 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dbpredict_pipes",
-    version="0.2.0",
+    version="0.3.0",
     author="Bryan Perry",
     author_email="bryan.perry@fticonsulting.com",
     description="End-user specific interface to prepare data for processing and prediction by dbpredict",
@@ -24,7 +24,7 @@ setuptools.setup(
     python_requires='>=3.7',
     install_requires=['pytest','datetime','python-dateutil',
                       'sqlalchemy','pandas','cx_Oracle',
-                      'pathlib','freezegun'],
+                      'pathlib','freezegun','tables'],
     keywords = 'diabetes prediction complications machine learning',
     project_urls = {
         'FTI CHEP' : 'https://www.fticonsulting.com/industries/healthcare-and-life-sciences/economics-and-policy',
